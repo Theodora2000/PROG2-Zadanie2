5e28d775c08fabd9e8997178fa0dab7b938c6c98
Theodora2000 <xsimont@stuba.sk>
2021-03-17 18:14:33 +0100

    Inicializácia závodu - 1

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #define R_MAX 2147483647 // vsetky generovane cisla su mensie ako R_MAX
 static long long unsigned int SEED = 0x1; // seed generatora
 
+
+
 void srnd(int seed) {
     SEED = seed;
 }
@@ -22,13 +24,61 @@ int main() {
     int N_min=10;
     int N_max=100;
 
+
     scanf("%d %d %d %d", &seed, &n, &m1, &m2);
 
     if(seed<=0 || n<N_min || n> N_max || ((m1+m2)>(n/2)) ){
         return 1;
     }
 
+    int zavodna_draha[n];
+
+    for(int i=0;i<n;i++){
+        zavodna_draha[i]=0;
+    }
+
     srnd(seed);
+
+    int pBooster=0;
+    int pBlocker=0;
+    int pozicia;
+    int pozecieBlocker[m1];
+
+
+    while(pBlocker!=m1){
+        pozicia = rnd(1,n-1);
+        if(zavodna_draha[pozicia-1]==0){
+            zavodna_draha[pozicia-1]= 1;
+            //printf("%d ", pozicia);
+            pozecieBlocker[pBlocker]= pozicia;
+            pBlocker++;
+        }else{
+            do{
+                pozicia = rnd(1,n-1);
+                //printf("\npozicia %d\n", pozicia);
+            }while(zavodna_draha[pozicia-1]==1);
+            zavodna_draha[pozicia-1]= 1;
+            //printf("%d ", pozicia);
+            pozecieBlocker[pBlocker]= pozicia;
+            pBlocker++;
+
+        }
+    }
+
+    //sort
+    for(int i=0;i<pBlocker-1;i++){
+        for(int j=1;j<pBlocker;j++){
+            if(i!=j){
+            if(pozecieBlocker[i]>pozecieBlocker[j]){
+                int temp = pozecieBlocker[i];
+                pozecieBlocker[i]  = pozecieBlocker[j];
+                pozecieBlocker[j] = temp;
+            }
+        }
+
+        }
+        printf("%d ", pozecieBlocker[i]);
+    }
 
     return 0;
 }
