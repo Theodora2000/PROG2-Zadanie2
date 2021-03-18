771ffc3444f6880c3daf6607526a49937b88990c
Theodora2000 <xsimont@stuba.sk>
2021-03-18 13:29:41 +0100

    Inicializácia závodu - final

--- main.c
+++ main.c
@@ -4,7 +4,20 @@
 #define R_MAX 2147483647 // vsetky generovane cisla su mensie ako R_MAX
 static long long unsigned int SEED = 0x1; // seed generatora
 
-
+int funk_na_zotredenie(int pole[], int typ, int zostalo, int zmensi){
+    do{
+        zostalo = 0;
+        for(int i=0;i<(typ-zmensi);i++){
+            if(pole[i]>pole[i+1]){
+                int temp = pole[i];
+                pole[i]=pole[i+1];
+                pole[i+1]=temp;
+                zostalo = 1;
+            }
+        }
+        zmensi++;
+    }while(zostalo);
+}
 
 void srnd(int seed) {
     SEED = seed;
@@ -43,46 +56,60 @@ int main() {
     int pBlocker=0;
     int pozicia;
     int pozecieBlocker[m1];
-
+    int pozecieBooster[m2];
 
     while(pBlocker!=m1){
         pozicia = rnd(1,n-1);
         if(zavodna_draha[pozicia-1]==0){
             zavodna_draha[pozicia-1]= 1;
-            //printf("%d ", pozicia);
             pozecieBlocker[pBlocker]= pozicia;
             pBlocker++;
         }else{
             do{
                 pozicia = rnd(1,n-1);
-                //printf("\npozicia %d\n", pozicia);
             }while(zavodna_draha[pozicia-1]==1);
             zavodna_draha[pozicia-1]= 1;
-            //printf("%d ", pozicia);
             pozecieBlocker[pBlocker]= pozicia;
             pBlocker++;
 
         }
     }
+
     //bouble sort
     int zostalo;
     int zmensi= 1;
-    do{
-        zostalo = 0;
-        for(int i=0;i<(pBlocker-zmensi);i++){
-            if(pozecieBlocker[i]>pozecieBlocker[i+1]){
-                int temp = pozecieBlocker[i];
-                pozecieBlocker[i]=pozecieBlocker[i+1];
-                pozecieBlocker[i+1]=temp;
-                zostalo = 1;
-            }
-        }
-        zmensi++;
-    }while(zostalo);
+    funk_na_zotredenie(pozecieBlocker, pBlocker, zostalo, zmensi);
 
+    while(pBooster!=m2){
+        pozicia = rnd(1,n-1);
+        if(zavodna_draha[pozicia-1]==0 && zavodna_draha[pozicia-1]!=1 && zavodna_draha[pozicia-1]!=2){
+            zavodna_draha[pozicia-1]= 2;
+            //printf("%d ", pozicia);
+            pozecieBooster[pBooster]= pozicia;
+            pBooster++;
+        }else{
+            do{
+                pozicia = rnd(1,n-1);
+            }while(zavodna_draha[pozicia-1]==1 || zavodna_draha[pozicia-1]==2);
+            zavodna_draha[pozicia-1]= 2;
+            pozecieBooster[pBooster]= pozicia;
+            pBooster++;
+
+        }
+    }
+
+    funk_na_zotredenie(pozecieBooster, pBooster, zostalo, zmensi);
+
+    printf("BLOCK:");
     for(int i=0;i<pBlocker;i++){
         printf("%d ", pozecieBlocker[i]);
     }
+
+    printf("\nBOOST:");
+    for(int i=0;i<pBooster;i++){
+        printf("%d ", pozecieBooster[i]);
+    }
+
 
 
 
